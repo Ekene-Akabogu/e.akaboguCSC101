1260dbcf23a0efc5d767291dcab564744d4f11bc
_.Zaki._ <ekenem.akabogu@gmail.com>
2022-10-23 17:48:46 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,5 @@
+fn main() {
+println!("hello");//prints
+println!("format {} arguments","some" );//prints format some arguments
+println!("My name is {}. I am the class rep of stream 1","Dagogo William-Jumbo");
+}
