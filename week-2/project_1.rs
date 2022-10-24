46a24cc783256069b916328867e7ecbeaa18fd62
_.Zaki._ <ekenem.akabogu@gmail.com>
2022-10-24 01:32:23 +0100

    (My second commit)

--- week-2/project_1.rs
+++ week-2/project_1.rs
@@ -2,9 +2,12 @@ fn main() {
 	let p:f64 = 520000000.0;
 	let r:f64 = 10.0;
 	let t:f64 = 5.0;
+	let m = 1.0 + (r / 100.0);
+	let k = f64:: powf(m,t);
 
-	let a = p * (1.0 + (r/100.0)) * t;
+	let a = p * k;
 	println!("Amount is {}",a);
+	
 	let ci = a - p;
 	println!("Compound Interest is {}",ci);
 }
