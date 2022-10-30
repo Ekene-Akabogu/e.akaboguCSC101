e91ae904f4ffb4dfde30a092db63ef1bfb2d97cc
_.Zaki._ <ekenem.akabogu@gmail.com>
2022-10-30 16:37:24 +0100

    (My third commit)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_charcter = '@'; //default
+    let alphabeth:char = 'B';
+    let surname = "Bolaji";
+    let first_name = "Michael";
+    let middle_name = "Gboyega";
+
+    println!(" ", );
+    println!("Special character: {}",special_charcter);
+    println!("Alphabeth: {}",alphabeth);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+}
